855b856c32c4ae8fd16b92f4e7a054ce15b1953c
anhcute1111 <bradles123pun0suy@hotmail.com>
2024-05-09 15:39:17 +0700

    update task

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function largestNumber(nums) {
-  if (nums.every((num) => num === 0)) return "0";
-  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+function getMin(arr) {
+  return Math.min(...arr);
 }
